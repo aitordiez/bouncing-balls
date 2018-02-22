616bca4ccfc1fdf78fd4e50e50ad3c1d856402cf
Aitor Diez Fernandez <aitorchic@gmail.com>
2018-02-22 01:30:04 +0100

    El usuario puede introducir por parametro cuantas bolas quiere que aparezcan en pantalla

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,43 +11,47 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private ArrayList<BouncingBall>bolas;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        bolas = new ArrayList<BouncingBall>();
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int balls)
     {
         int ground = 400;   // position of the ground line
 
         myCanvas.setVisible(true);
-
+       
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
+        for(int i=0; i<balls; i++){
+            BouncingBall bolass= new BouncingBall(60 + (50*i), 60, 50, Color.GREEN, ground, myCanvas);
+            bolas.add(bolass);
+            bolass.draw();
+        }
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
+             for(int i=0; i<balls; i++){
+                 bolas.get(i).move();
+             }
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+            /*if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
                 finished = true;
-            }
+            }*/
+            
         }
     }
 }
+
